7bbd947f965be50acd2c8b922d9cb95918a2c192
Lucy Chyzhova <liudmyla.chyzhova@gmail.com>
2020-09-01 13:22:01 +0200

    added loading and no results message

--- react/week4/src/components/DataFetching.jsx
+++ react/week4/src/components/DataFetching.jsx
@@ -1,34 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 function DataFetching() {
-const [user, setUser] = useState({})
-const [login, setLogin] = useState("")
-const [idFromButtonClick, setIdFromButtonClick] = useState("")
+  const [user, setUser] = useState({});
+  const [login, setLogin] = useState("");
+  const [idFromButtonClick, setIdFromButtonClick] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-const handleClick = () => {
-    setIdFromButtonClick(login)
-}
+  const handleClick = () => {
+    setIdFromButtonClick(login);
+  };
 
-  useEffect(() =>{
-      axios
-      .get (`https://api.github.com/users/${idFromButtonClick}`)
-      .then(res => {
-          console.log(res)
-          setUser(res.data)
-      })
-      .catch (err => {
-          "console.log(err)"
-      })
-  }, [idFromButtonClick])  
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(
+          `https://api.github.com/users/${idFromButtonClick}`
+        );
+        setUser(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [idFromButtonClick]);
 
   return (
-<div>
-<input placeholder={"Search"} type="test" value={login} onChange={e =>setLogin(e.target.value)} />
-<button type="button" onClick={handleClick}>Search</button>
-<div>{user.id}</div>
-</div>
-  )
+    <Fragment>
+      <div>
+        <input
+          placeholder={"Search"}
+          type="test"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <button type="button" onClick={handleClick}>
+          Search
+        </button>
+
+        {isError && <div>No results ...</div>}
+        {isLoading ? <div>Loading ...</div> : <div>{user.id}</div>}
+      </div>
+    </Fragment>
+  );
 }
 
 export default DataFetching;
