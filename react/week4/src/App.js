131e55fc48b6e5643ecd1db46443ebf6e27554fb
Lucy Chyzhova <liudmyla.chyzhova@gmail.com>
2020-09-03 13:37:51 +0200

    added page About and CSS style, cleaned up an if conditions

--- react/week4/src/App.js
+++ react/week4/src/App.js
@@ -1,21 +1,37 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
-import {Search} from "./pages/Search";
-
-
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Search } from "./pages/Search";
+import { About } from "./pages/About";
 
 export const App = () => {
   return (
     <BrowserRouter>
-    <div className="navigation">
-      <Link to="/search">Search</Link>
-    </div>
-    <Switch>
-      <Route path="/search">
-        <Search/>
-      </Route>
-    </Switch>
+      <div className="navigation">
+        <div className="link">
+          <Link to="/search">Search</Link>
+        </div>
+        <div className="link">
+          <Link to="/about">About</Link>
+        </div>
+      </div>
+      <Link to="/">
+        <img
+          className="github-logo"
+          src="https://live.staticflickr.com/65535/50294062736_72e0ed96b4_c.jpg"
+          alt="github_logo"
+          width="25%"
+        />
+      </Link>
+
+      <Switch>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 export default App;
--- react/week4/src/components/Fetcher.js
+++ react/week4/src/components/Fetcher.js
@@ -3,35 +3,33 @@ import React, { useState, useEffect } from "react";
 const baseUrl = "https://api.github.com/search/users?q=";
 
 export const Fetcher = ({ search, render }) => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const getData = async () => 
-    {
-      const result = await fetch(
-        `${baseUrl}${search}`
-      ).then((response) => {
+    const getData = async () => {
+      setLoading(true);
+      const result = await fetch(`${baseUrl}${encodeURIComponent(search)}`).then((response) => {
         return response.json();
       });
 
-     if (search === ""){
-        setLoading(false);
+      setLoading(false);
+
+      if (result.error) {
         setError(result);
-        setData(false);
-      }
-      else if (search !== "") {
-        setLoading(false);
-        setData(result.items); 
+      } else {
+        setData(result.items);
         setError(false);
       }
-      else {
-        setLoading(true);
-        setData(result.items);
-      }
     };
-    getData();
+
+    if (search !== "") {
+      getData();
+    } else {
+      setError(true);
+      setData(false);
+    }
   }, [search]);
 
   return (
